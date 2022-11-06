1aa8cde0d31d8946b4f0f6c3186eba5f27c60a66
bipentihexium <bipentihexium@gmail.com>
2022-11-06 22:57:58 +0100

    Rewrite cproxy.py

--- cproxy.py
+++ cproxy.py
@@ -2,19 +2,40 @@ import argparse
 import os
 import sys
 
-parser = argparse.ArgumentParser(prog="cproxy", description="a proxy between cpp_build_utils and CMake")
-parser.add_argument("action", choices=["build", "configure", "gen", "install", "new", "new_json", "test"], type=str, help="Action to perform")
-parser.add_argument("action_args", type=str, help="Action arguments", nargs='*')
-parser.add_argument("--version", action="version", version="%(prog)s v1.0.0.0", help="Show version")
-parser.add_argument("-v", "--verbose", action="store_true", help="show ran commands")
-parser.add_argument("--mgw", action="store_true", help="use MinGW Makefiles as generator")
-parser.add_argument("--debug", action="store_true", help="do debug build (for configure!)")
-parser.add_argument("--release", action="store_true", help="do release build (for configure!)")
-parser.add_argument("--tests", action="store_true", help="build tests (for configure!)")
-args = parser.parse_args()
+for a in sys.argv[1:]:
+	if not a.startswith("-"):
+		action = a
+		break
+else:
+	print("No action specified!")
+	print("help:")
+	print("cproxy - a proxy between cpp_build_utils and CMake")
+	print("Usage:")
+	print("\tcproxy <action> ... [options]")
+	print("Actions:")
+	print("\tbuild - builds current cmake project")
+	print("\tconfigure/conf - configure current cmake project")
+	print("\tcmgen/gen - generate cmake project from cmgen.json and configure it")
+	print("\tinstall - install built project")
+	print("\tnew <name> - crates new cmake project")
+	print("\tnew_cmg <name> - creates new cmgen project")
+	print("\ttest - test current project; options are forwarded to ctest, so check ctest's help for this; but it runs all tests by default")
+	print("Global options:")
+	print("\t--verbose  -v   show ran commands (doesn't work with cproxy test)")
+	print("Options:")
+	print("\tbuild - run cproxy build -h to see it's help")
+	sys.exit(1)
+
+if action in ["new", "new_cmg"]:
+	aargs = [a for a in sys.argv[1:] if not a.startswith('-')]
+	if aargs.size() < 2:
+		print("cproxy " + action + "<name> needs a name argument")
+	pname = aargs[1]
+
+verbose = "--verbose" in sys.argv or any(a.startswith('-') and 'v' in a for a in sys.argv)
 
 def cmd(c):
-	if args.verbose:
+	if verbose:
 		print(f"cproxy> {c}")
 	os.system(c)
 
@@ -40,40 +61,40 @@ def gen_project(project, template_dir):
 	ppath = os.getcwd() + "/" + project
 	tpath = os.path.dirname(os.path.realpath(__file__)) + "/" + template_dir
 	copy_proj_template(tpath, ppath, project)
+def configure(desc):
+	parser = argparse.ArgumentParser(prog=f"cproxy {action}", description=desc)
+	parser.add_argument("action", type=str, help="The action - conf/configure")
+	parser.add_argument("--mgw", action="store_true", help="use MinGW Makefiles as generator")
+	parser.add_argument("--debug", action="store_true", help="do debug build (for configure!)")
+	parser.add_argument("--release", action="store_true", help="do release build (for configure!)")
+	parser.add_argument("--tests", action="store_true", help="build tests (for configure!)")
+	parser.add_argument("-v", "--verbose", action="store_true", help="show ran commands")
+	args = parser.parse_args()
+	cmake_args = ""
+	if args.debug:
+		cmake_args += " -DCMAKE_BUILD_TYPE=Debug"
+	elif args.release:
+		cmake_args += " -DCMAKE_BUILD_TYPE=Release"
+	if args.mgw:
+		cmake_args += " -G\"MinGW Makefiles\""
+	if args.tests:
+		cmake_args += " -DBUILD_TESTING=ON"
+	cmd(f"cmake -S . -B build{cmake_args}")
 
-bt = ""
-if args.debug:
-	bt = " -DCMAKE_BUILD_TYPE=Debug"
-elif args.release:
-	bt = " -DCMAKE_BUILD_TYPE=Release"
-g = ""
-if args.mgw:
-	g = " -G\"MinGW Makefiles\""
-bo = ""
-if args.tests:
-	bo = " -DBUILD_TESTING=ON"
-
-
-if args.action == "build":
+if action == "build":
 	cmd("cmake --build build")
-elif args.action == "configure":
-	cmd(f"cmake -S . -B build{g}{bt}{bo}")
-elif args.action == "gen":
+elif action == "configure" or action == "conf":
+	configure("Configure current cmake project")
+elif action == "cmgen" or action == "gen":
 	cmd(sys.executable + " " + os.path.dirname(os.path.realpath(__file__)) + "/cmake_gen.py ./cmgen.json ./")
-	cmd(f"cmake -S . -B build{g}{bt}{bo}")
-elif args.action == "install":
+	configure("Generate CMake project from cmgen.json and configure it")
+elif action == "install":
 	cmd("cmake --install build")
-elif args.action == "new":
-	if not args.action_args:
-		parser.error("'new' needs an argument (cproxy new <project_name>)")
-	project = args.action_args[0]
-	gen_project(project, "default_cmake_proj")
-elif args.action == "new_json":
-	if not args.action_args:
-		parser.error("'new' needs an argument (cproxy new <project_name>)")
-	project = args.action_args[0]
-	gen_project(project, "default_cmgen_proj")
-elif args.action == "test":
+elif action == "new":
+	gen_project(pname, "default_cmake_proj")
+elif action == "new_cmg":
+	gen_project(pname, "default_cmgen_proj")
+elif action == "test":
 	os.chdir("./build")
-	cmd("ctest")
+	cmd("ctest " + " ".join(sys.argv[2:]))
 	os.chdir("../")
